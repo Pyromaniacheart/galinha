2843acd2c651346a65984c578d31d1e33007c0ca
Pyromaniacheart <ssophiasbampato@gmail.com>
2026-05-04 20:11:54 +0000

    Inserção de informação no HTML

--- PORTFOLIO/SCRIPTS/script.js
+++ PORTFOLIO/SCRIPTS/script.js
@@ -1,1 +1,28 @@
-const botão = document.getElementById(modoClaroModoescuro)
+const botão = document.getElementById(modoClaroModoescuro);
+
+const NOME = "Anderson Bernardo";
+let tituloProfissional = "Garoto de Programa / Faço de Tudo";
+let minhaBio = "sou um cara esforçado. Agora sou baterista" +
+ "Antes eu não era nada."
+let anoFormatura = 2028;
+let anoIngresso = 2025;
+let indefinido;
+let nulo = null;
+let curso = {
+    nome: "Sistemas de Informação",
+    semestre: 3,
+    disciplinaAtual: "Design focado no usuário"
+};
+
+console.log(typeof nulo);
+console.log(typeof indefinido);
+console.log(typeof anoFormatura);
+console.log(typeof minhaBio);
+console.log(typeof tituloProfissional);
+console.log(typeof NOME);
+console.log(typeof curso);
+
+document.getElementById("meuNome").innerText = NOME;
+document.getElementById("tituloProfissional").innerText = tituloProfissional;
+document.getElementById("minhaBio").innerText = minhaBio;
+document.getElementById("anoFormatura").innerText = anoFormatura;
